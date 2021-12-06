1fb5e9e1597b8095ee337eaa12231c427cde9c81
Luis Coelho <mancoeluis@gmail.com>
2021-12-06 20:34:34 -0300

    Fixed errors and added routes
    Fixed some errors when adding and listing users and moved endpoints to the routes file.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// O arquivo index.js contém somente as configurações de servidor:
+
+const express = require('express') // Importando o framework express
+const routes = require('./routes') // Importando o arquivo 'routes.js' que se encontra na mesma pasta (src)
+const server = express() // Utilizando o express numa variável para que possamos fazer os comandos abaixo
+const port = 8080 // Definindo a porta como uma variável
+
+server.use(express.json()) // Prepara a API para lidar com arquivos em formato json
+
+server.use('/', routes) // A API usará os endpoints definidos no arquivo routes.js 
+
+server.listen(port, () => { // A API usará a porta definida
+    console.log('Servidor rodando na porta '+ port) // Mensagem que aparecerá quando o servidor estiver rodando
+})
